18f0cf607324a02ce68397cc8d46586154065513
Stuart Irwin <stuin01@gmail.com>
2021-12-06 21:49:17 -0800

    Day 7 and fix day 3

--- 3Diagnostic.kt
+++ 3Diagnostic.kt
@@ -1,43 +1,28 @@
 package Event4_AOC
 
 import java.io.File
-import java.util.*
 
 fun main(args: Array<String>) {
-    val reader = File(args[0])
-    var common = arrayOf(0)
-    var size = 0
-    var count = 0
+    val reader = File(args[0]).bufferedReader()
 
-    reader.forEachLine {
-        if(size == 0) {
-            common = Array(it.length) { 0 }
-            size = it.length
-        }
+    var oxygen: List<String> = reader.lines().toArray().filterIsInstance<String>()
+    var scrubber = oxygen
 
-        for(i in common.indices)
-            common[i] += it[i].digitToInt()
-        count++
+    var i = 0
+    while(oxygen.size > 1) {
+        val common = if(oxygen.filter { it[i] == '1' }.size >= oxygen.filter { it[i] == '0' }.size) '1' else '0'
+        oxygen = oxygen.filter { it[i] == common }
+        i++
     }
 
-    val gamma = BitSet(size)
-    val epsilon = BitSet(size)
-
-    for(i in common.indices) {
-        gamma[size-i-1] = common[i] > count / 2
-        epsilon[size-i-1] = common[i] < count / 2
-        print("${common[i]} ${gamma[size-i]} ")
+    i = 0
+    while(scrubber.size > 1) {
+        val common = if(scrubber.filter { it[i] == '1' }.size < scrubber.filter { it[i] == '0' }.size) '1' else '0'
+        scrubber = scrubber.filter { it[i] == common }
+        i++
     }
 
-    var a = 0L
-    var b = 0L
-
-    for (i in 0 until gamma.length()) {
-        a += if(gamma.get(i)) (1L shl i) else 0L
-        b += if(epsilon.get(i)) (1L shl i) else 0L
-    }
-
-    println()
-    println("$a * $b")
-    println(a * b)
+    println(oxygen[0])
+    println(scrubber[0])
+    println(oxygen[0].toInt(2) * scrubber[0].toInt(2))
 }
---
+++ 7Treachery.kt
@@ -0,0 +1,21 @@
+package Event4_AOC
+
+import java.io.File
+import kotlin.math.abs
+
+fun main(args: Array<String>) {
+    val reader = File(args[0]).bufferedReader()
+
+    val line = reader.readLine().split(",").map { it.toInt() }
+    val min = line.minOrNull()
+    val max = line.maxOrNull()
+
+    if(min == null || max == null)
+        return;
+
+    val positions = (min..max).map { pos ->
+        line.sumOf { (1..abs(it - pos)).sum() }
+    }
+
+    println(positions.minOrNull())
+}
